d9336f9e725989fbd35384b0eb22cc81c15d6cd8
Alan O'Donohoe <alan@odonohoe.org.uk>
2015-09-13 15:42:44 +0100

    Update 2Mb_generator.py

--- 2Mb_generator.py
+++ 2Mb_generator.py
@@ -2,7 +2,7 @@
 
 import random
 
-for a in range (2000000):
-    number = random.randint(0,1)
-    print(number, end=" ")
+for size in range (2000000):
+    bit = random.randint(0,1)
+    print(bit, end=" ")
     
